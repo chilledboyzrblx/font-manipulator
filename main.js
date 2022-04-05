1cd07c76cb66728e73a1db75e9d9672680bb9d39
Mr.GeschVed <86187817+chilledboyzrblx@users.noreply.github.com>
2022-04-05 14:36:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    video.position(50,150);
+
+    canvas = createCanvas(550,500);
+    canvas.position(750,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('MODEL HAS LOADED');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
